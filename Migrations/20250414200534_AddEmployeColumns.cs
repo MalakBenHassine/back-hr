e79d77af5f792922f58cd3ddd48864fa6c6ba1c7
BadiiMsalmi <badiimsalmi2@gmail.com>
2025-04-15 11:38:05 +0100

    correcting migrations and updating gitignore

---
+++ Migrations/20250414200534_AddEmployeColumns.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Back_HR.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmployeColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Absences",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClientSatisfactionScore",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "HireDate",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "LateArrivals",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "OnTimeCompletionRate",
+                table: "Employees",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProcessImprovementIdeas",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TasksCompleted",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Absences",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ClientSatisfactionScore",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "HireDate",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "LateArrivals",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "OnTimeCompletionRate",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ProcessImprovementIdeas",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "TasksCompleted",
+                table: "Employees");
+        }
+    }
+}
---
+++ Migrations/20250414202310_AddAdequacyScoreToApplications.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Back_HR.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdequacyScoreToApplications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AdequacyScore",
+                table: "Applications",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("AdequacyScore", "Applications");
+        }
+    }
+}
